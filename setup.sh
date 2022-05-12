21c0fd8bd5fa651a061992ac483489284858d806
sabaferdous12 <saba.ferdous.13@ucl.ac.uk>
2022-05-12 15:49:00 +0100

    trying ti fix jupytext issue

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+pip install jupytext
 
 mkdir -p ~/.virtualenvs/
 
@@ -8,6 +9,6 @@ source ~/.virtualenvs/r-env/bin/activate
 
 pip install -r requirements.txt
 
-sudo apt update -y 
+sudo apt update -y
 
 sudo apt install -y texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra
